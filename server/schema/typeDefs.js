984ed4b8b5b5ed7f2f2a4299817dde40bc95b78b
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2021-12-13 18:29:49 +0000

    chore: inserted bookInput in type def
    Authors : NR

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -20,6 +20,14 @@ const typeDefs = gql`
     token: ID!
     user: User
   }
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
   type Query {
     me: User
     users: [User]
@@ -27,14 +35,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(
-      authors: [String]
-      description: String
-      title: String
-      bookId: String
-      image: String
-      link: String
-    ): User
+    saveBook(bookData: BookInput!): User
     removeBook(bookId: String): User
   }
 `;
