560ad573212bb3d37e5b41aca5a6cfb353251e18
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2021-12-13 18:28:03 +0000

    chore: changed input into bookData
    Authors: nr

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -83,7 +83,7 @@ const SearchBooks = () => {
     try {
       const response = await saveBook({
         variables: {
-          input: bookToSave,
+          bookData: bookToSave,
         },
       });
 
